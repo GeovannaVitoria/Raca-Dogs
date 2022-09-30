71751e159d9bdcfa24817e8fa9ffbd471e6e3aa1
GeovannaVitoria <geovannavitrj@gmail.com>
2022-09-29 21:50:13 -0300

    Raca-Dogs

---
+++ main.js
@@ -0,0 +1,16 @@
+function logo(){
+    var logo = document.getElementById('logo');
+    logo.innerHTML = `<img src="logoremoved.png">`
+    // logo.style.color = "red";
+}
+
+function out(){
+    var logo = document.getElementById('logo');
+    logo.innerHTML = `<h1> Raça Dogs </h1>`
+}
+
+function photo1(){
+    var photo = document.getElementById('photo1');
+    photo.style = "none"
+    photo.innerHTML = `<p> Olá, primeiro click image here </p>` 
+}
